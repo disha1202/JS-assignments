59f19fd18d96ac1942e71dbffa03172cb0c91c36
disha1202 <58461801+disha1202@users.noreply.github.com>
2021-07-21 16:45:17 +0530

    Adding task2 of module 7

---
+++ Module 7/task2/app.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("td").forEach(e => e.addEventListener("click",function(){
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    e.style.backgroundColor = bgColor;
+}));
