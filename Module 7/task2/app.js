6fd811f70a01a332874672d6409d0815dff0c1ae
disha1202 <58461801+disha1202@users.noreply.github.com>
2021-07-21 16:48:03 +0530

    Update app.js

--- Module 7/task2/app.js
+++ Module 7/task2/app.js
@@ -1,4 +1,4 @@
-document.querySelectorAll("td").forEach(e => e.addEventListener("click",function(){
+document.querySelectorAll("td").forEach(x => x.addEventListener("click",function(){
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
