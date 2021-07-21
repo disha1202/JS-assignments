bfd2e4df15da8916f27df75a2569388c5dcf0667
disha1202 <58461801+disha1202@users.noreply.github.com>
2021-07-21 16:48:50 +0530

    Update app.js

--- Module 7/task2/app.js
+++ Module 7/task2/app.js
@@ -1,7 +1,7 @@
-document.querySelectorAll("td").forEach(x => x.addEventListener("click",function(){
+document.querySelectorAll("td").forEach(a => a.addEventListener("click",function(){
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    e.style.backgroundColor = bgColor;
+    a.style.backgroundColor = bgColor;
 }));
